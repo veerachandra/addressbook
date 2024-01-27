c34d2664125e9e914bf00ace9b5afb4e9443c7c0
veerachandra <veerachandra.solige@gmail.com>
2024-01-27 21:09:12 +0530

    changes test-practice

--- server-script.sh
+++ server-script.sh
@@ -9,10 +9,10 @@ then
    echo "repo is already cloned and exists"
    cd /home/ec2-user/addressbook
    git checkout test-practice
-   git pull origin  test-practice
+   git pull origin test-practice
 else
    git clone https://github.com/veerachandra/addressbook.git
    git checkout test-practice
 fi
-
+echo "test-practice repo got updated using git pull origin test-practice"
 mvn package
