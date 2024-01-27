31fb37d58fe6b7a1b53b20c00bf65067e9d19a74
veerachandra <veerachandra.solige@gmail.com>
2024-01-27 21:24:31 +0530

    veera-addressbook

--- server-script.sh
+++ server-script.sh
@@ -4,13 +4,15 @@ sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
 
-if [ -d "addressbook" ]
+if [ -d "veera-addressbook" ]
 then
    echo "repo is already cloned and exists"
    cd /home/ec2-user/veera-addressbook
    git checkout test-practice
    git pull origin test-practice
 else
+   echo "first time repo is cloned and veera-addressbook directory"
+   mkdir /home/ec2-user/veera-addressbook
    cd /home/ec2-user/veera-addressbook
    git clone https://github.com/veerachandra/addressbook.git
    git checkout test-practice
